0195e90bbd3964b8901c76b86804c46d133f3d81
ANDREEA-MARIA LAZAR <andreea.maria.lazar@stud.ubbcluj.ro>
2021-01-17 20:51:51 +0200

    Add project files.

---
+++ Lazar_Andreea_lab12/Lazar_Andreea_lab12/Data/IRestService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Lazar_Andreea_lab12.Models;
+
+namespace Lazar_Andreea_lab12.Data
+{
+    public interface IRestService
+    {
+        Task<List<ShopList>> RefreshDataAsync();
+        Task SaveShopListAsync(ShopList item, bool isNewItem);
+        Task DeleteShopListAsync(int id);
+    }
+}
